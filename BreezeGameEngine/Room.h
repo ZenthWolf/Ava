cb114f7a92dafa8ef5db6e3dcd55fc819f0767d1
ZenthWolf <ZenthRelin@gmail.com>
2020-08-30 15:15:17 -0400

    Some cleanup from merge

--- BreezeGameEngine/Room.h
+++ BreezeGameEngine/Room.h
@@ -27,11 +27,6 @@ private:
 	void ReadInput() const;
 	void EnemyCollision(); //Check if Ava is in contact with an enemy
 	void HitDetection(); //Detect Collision of attacks
-	void Cull();
-
-	Character& Ava; //Does Ava properly belong to the "Room" she is in? Or do rooms just "know" about her?
-	std::vector<std::unique_ptr<Enemy>> enemy; // Should have a list of enemies- ALL ENEMIES ARE REQUIRED TO DO ANYTHING STATED HERE, SO BASIS FOR SUBCLASS!
-	std::vector<std::unique_ptr<Obstacle>> obstacle; // List of obstacles- which will eventually be a class like "ground" once was.
 
 	Keyboard& kbd;
 };
