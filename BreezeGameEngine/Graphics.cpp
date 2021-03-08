fb42303adc27e783e8f47dddf13a20a852cf5498
ZenthWolf <ZenthRelin@gmail.com>
2021-03-07 19:39:38 -0500

    DrawRect no longer misses the furthest pixel lines

--- BreezeGameEngine/Graphics.cpp
+++ BreezeGameEngine/Graphics.cpp
@@ -317,9 +317,9 @@ void Graphics::DrawRect(int x0, int y0, int x1, int y1, Color c)
 	}
 
 
-	for (int x = x0; x < x1; x++)
+	for (int x = x0; x <= x1; x++)
 	{
-		for (int y = y0; y < y1; y++)
+		for (int y = y0; y <= y1; y++)
 		{
 			PutPixel(x, y, c);
 		}
